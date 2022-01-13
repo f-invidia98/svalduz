c4fe5d87781dc0d17f8dcd7c07345642401f49f1
f-invidia98 <francesco.invidia.98@gmail.com>
2022-01-13 21:52:13 +0100

    ok

--- svalduz_script.js
+++ svalduz_script.js
@@ -1,10 +1,11 @@
-
 var hash = window.location.hash.substr(1);
-var result= hash.split('&');
+var result = hash.split('&');
+
+var slideIndex = 1;
 
 
 
-$("#archivio-toggle").click(function(){
+$("#archivio-toggle").click(function() {
   archiv();
 });
 
@@ -18,7 +19,7 @@ function archiv() {
 };
 
 
-$("#studio-toggle").click(function(){
+$("#studio-toggle").click(function() {
   $("#archivio-menu").css("display", "none")
   $(".pos").css("line-height", "16pt")
   $(".pos").addClass("padding_top")
@@ -27,7 +28,7 @@ $("#studio-toggle").click(function(){
 
 })
 
-$(".indietro").click(function(){
+$(".indietro").click(function() {
   $("#archivio-menu").css("display", "inline-block")
   $("#studio-menu").css("display", "inline-block")
   $(".pos").css("line-height", "100vh")
@@ -39,50 +40,166 @@ $(".indietro").click(function(){
   $(".menu-archivio").css("display", "none")
   remove_hash_from_url()
   $(".schede").css("display", "none")
-  $(".immagine").addClass("hide")
+  $(".scheda_hide").addClass("hide")
   $(".menu-archivio > div").removeClass("blue")
   $(".menu-archivio > div").removeClass("hide")
   $(".arrow").addClass("hide")
 
 })
 
-$(".legal-toggle").click(function(){
+$(".legal-toggle").click(function() {
   $(".studio-info").css("display", "none")
   $(".legal-info").css("display", "inline-block")
 })
 
-$(".legal-info").click(function(){
+$(".legal-info").click(function() {
   $(".studio-info").css("display", "inline-block")
   $(".legal-info").css("display", "none")
 })
 
 var stato = 0;
 
-$(".scheda").click(function(){
-  if (!$(this).children(".immagine").hasClass("hide")) {
-    $(this).children(".immagine").addClass("hide")
+$(".scheda_click").click(function() {
+
+  if ($(this).parentsUntil(".schede").hasClass("lui")) {
+    $(".scheda_click").addClass("hide");
+  }
+
+
+  if (!$(this).parent().children(".scheda_hide").hasClass("hide")) {
+    $(this).parent().children(".scheda_hide").addClass("hide")
     remove_hash_from_url()
     window.location = "#" + clas;
-  } else if ($(this).children(".immagine").hasClass("hide")) {
-    $(".immagine").addClass("hide")
-    $(this).children(".immagine").removeClass("hide")
-      window.location = "#" + clas + "&" + this.id;
-      hash = window.location.hash.substr(1);
-      result = hash.split('&')
   }
+  else if ($(this).parent().children(".scheda_hide").hasClass("hide")) {
+    $(".scheda_hide").addClass("hide")
+    $(this).parent().children(".scheda_hide").removeClass("hide")
+    window.location = "#" + clas + "&" + $(this).parent().attr('id');
+    hash = window.location.hash.substr(1);
+    result = hash.split('&')
+  }
+
+
+
 });
 
 var clas;
 var id;
 
-$(".menu-archivio > div").click(function(){
+$(".menu-archivio > div").click(function() {
   clas = this.id.replace(/m/g, 'a');
   getLink(clas);
 })
 
-if (result!=0) {
+if (result != 0) {
   clas = result[0];
   getLink(result[0], result[1]);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var xcheck;
+
+jQuery(function($) {
+  var currentMousePos = {
+    x: -1,
+    y: -1
+  };
+  $(document).mousemove(function(event) {
+    currentMousePos.x = event.pageX;
+    currentMousePos.y = event.pageY;
+  });
+
+
+
+
+  $(document).ready(function() {
+    $(document).mousemove(function() {
+      if ($(".immagini:hover").length != 0) {
+        if (currentMousePos.x > $(".immagini:hover").width() / 2) {
+          $('.immagine').css('cursor', 'url("Assets/NEXT.png"), auto');
+          xcheck = 1;
+        } else if (currentMousePos.x < $(".immagini:hover").width() / 2) {
+          $('.immagine').css('cursor', 'url("Assets/PREV.png"), auto');
+          xcheck = 0;
+        }
+      }
+    });
+  });
+  // ELSEWHERE, your code that needs to know the mouse position without an event
+
+});
+
+
+var slides;
+
+$('.immagine').click(function() {
+  slides = $(this).parent().children(".immagine");
+  jQuery(function($) {
+    var currentMousePos = {
+      x: -1,
+      y: -1
+    };
+    $(document).mousemove(function(event) {
+      currentMousePos.x = event.pageX;
+      currentMousePos.y = event.pageY;
+    });
+
+
+
+
+    if (xcheck == 1) {
+      slideIndex += 1
+      showSlides(slideIndex, slides)
+      console.log(slideIndex)
+    }
+    else if (xcheck == 0) {
+      slideIndex -= 1
+      showSlides(slideIndex, slides)
+      console.log(slideIndex)
+    }
+
+
+
+
+    // ELSEWHERE, your code that needs to know the mouse position without an event
+
+  });
+})
+
+
+
+
+function showSlides(n, slides) {
+  var i;
+
+  // var dots = $(".dot");
+  if (n > slides.length) {
+    slideIndex = 1
+  }
+  if (n < 1) {
+    slideIndex = slides.length
+  }
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  // for (i = 0; i < dots.length; i++)
+  // {
+  //   dots[i].className = dots[i].className.replace(" active", "");
+  // }
+  slides[slideIndex - 1].style.display = "block";
+  // dots[slideIndex - 1].className += " active";
 }
 
 $(window).resize(function() {
@@ -101,7 +218,8 @@ $(window).resize(function() {
     $(".indice").css("display", "inline-block")
     $(".menu-archivio > div").removeClass("hide")
     $(".arrow").addClass("hide")
-  }else if ($(window).width() <= 551) {
+  }
+  else if ($(window).width() <= 551) {
     $(".indice").css("display", "none")
     $(".menu-archivio > div").addClass("hide")
     var id2 = result[0].replace(/a/g, '');
@@ -111,14 +229,14 @@ $(window).resize(function() {
 
 })
 
-function getLink(anno, num, id){
+function getLink(anno, num, id) {
 
 
 
   archiv();
   $(".schede").css("display", "none")
   $("#" + anno).css("display", "block")
-  $(".immagine").addClass("hide")
+  $(".scheda_hide").addClass("hide")
   id = anno.replace(/a/g, '');
   $(".menu-archivio > div").removeClass("blue")
   $("#m" + id).addClass("blue")
@@ -127,7 +245,8 @@ function getLink(anno, num, id){
     $(".indice").css("display", "inline-block")
     $(".menu-archivio > div").removeClass("hide")
     $(".arrow").addClass("hide")
-  }else if ($(window).width() <= 551) {
+  }
+  else if ($(window).width() <= 551) {
     $(".indice").css("display", "none")
     $(".menu-archivio > div").addClass("hide")
     $("#m" + id).removeClass("hide")
@@ -135,7 +254,7 @@ function getLink(anno, num, id){
   }
 
 
-  $(".arrow").click(function(){
+  $(".arrow").click(function() {
     remove_hash_from_url()
     $(".menu-archivio > div").removeClass("hide")
     $(".arrow").addClass("hide")
@@ -146,12 +265,13 @@ function getLink(anno, num, id){
 
 
 
+
   window.location = "#" + anno;
 
-  if (num!=null) {
-    $(".immagine").addClass("hide")
-    $("#" + num).children(".immagine").removeClass("hide")
-    window.location ="#" + anno + "&" + num;
+  if (num != null) {
+    $(".scheda_hide").addClass("hide")
+    $("#" + num).children(".scheda_hide").removeClass("hide")
+    window.location = "#" + anno + "&" + num;
   }
 
 
@@ -175,20 +295,96 @@ function getLink(anno, num, id){
 
 
 function remove_hash_from_url() {
-            var uri = window.location.toString();
+  var uri = window.location.toString();
 
-            if (uri.indexOf("#") > 0) {
-                var clean_uri = uri.substring(0,
-                                uri.indexOf("#"));
+  if (uri.indexOf("#") > 0) {
+    var clean_uri = uri.substring(0,
+      uri.indexOf("#"));
 
-                window.history.replaceState({},
-                        document.title, clean_uri);
-                        hash = window.location.hash.substr(1);
-                        result = hash.split('&')
+    window.history.replaceState({},
+      document.title, clean_uri);
+    hash = window.location.hash.substr(1);
+    result = hash.split('&')
 
-            }
-        }
+  }
+}
 
+
+
+$(".full").click(function() {
+  if ($(".fullscreen").hasClass("hide")) {
+    $(".fullscreen").removeClass("hide")
+    $(".indice_center").html($(this).parentsUntil("scheda").find(".nome").html()+" ↓")
+    $(".indice_right").removeClass("hide")
+    $(".cantieriAperti").addClass("hide")
+
+  }
+
+  if (!$(".immagine").hasClass("immagine_big")) {
+    $(".immagine").addClass("immagine_big")
+  }
+})
+
+$(".fullscreen").click(function(){
+    exitFullscreen();
+})
+
+$(".red").click(function(){
+  exitFullscreen();
+})
+
+function exitFullscreen() {
+  if (!$(".fullscreen").hasClass("hide")) {
+    $(".fullscreen").addClass("hide")
+    $(".immagine").removeClass("immagine_big")
+    $(".indice_center").html("Indice ↓")
+    $(".indice_right").addClass("hide")
+    $(".cantieriAperti").removeClass("hide")
+  }
+}
+
+
+
+setInterval(highlightBlock, 3000); // Every two seconds
+
+function highlightBlock() {
+      $('.cantieriAperti').css('color', '#0000FF'); // Change colour
+      setTimeout(function() {
+             $('.cantieriAperti').css('color', 'black'); // Change back
+      }, 1500);
+}
+
+
+
+
+
+
+//   $(".immagine").click(function(){
+//     // $(".lab_image").css("transform", "translate(0vw, 0)")
+//     $("").removeClass("hide")
+//     $(".1").addClass("inverted")
+//   });
+//
+//
+
+
+
+// // Get the last <li> element ("Milk") of <ul> with id="myList2"
+// var itm = $(".scheda_hide").children();
+//
+// // Copy the <li> element and its child nodes
+// var cln = document.getElementByClass("scheda_hide").children(".immagini").cloneNode(true);
+//
+// // Append the cloned <li> element to <ul> with id="myList1"
+// $(".fullscreen").appendChild(cln);
+
+
+
+// if (!$(".fullscreen").hasClass(".hide")) {
+//   $(".immagine").addClass(".hide")
+// } else if ($(".immagine").hasClass(".fullscreen")) {
+//
+// }
 
 
 
