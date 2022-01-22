46bcecf579c6765aadc354b3a6a34b109b1ebdfc
f-invidia98 <francesco.invidia.98@gmail.com>
2022-01-22 12:49:42 +0100

    prova

--- svalduz_script_02.js
+++ svalduz_script_02.js
@@ -9,6 +9,18 @@ var slides;
 
   var xcheck;
 
+
+
+  let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
+
 $(window).on('popstate', function(event) {
  if (state.scheda == '0') {
    remove_hash_from_url();
