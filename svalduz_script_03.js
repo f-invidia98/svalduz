9b9d5da8a70c52d3332c65231321508257b0f6fb
f-invidia98 <francesco.invidia.98@gmail.com>
2022-11-18 13:18:19 +0100

    d

--- svalduz_script_03.js
+++ svalduz_script_03.js
@@ -19,6 +19,8 @@ var slides;
 
 
 
+
+
   let vh = window.innerHeight * 0.01;
 // Then we set the value in the --vh custom property to the root of the document
 document.documentElement.style.setProperty('--vh', `${vh}px`);
